b0b5e29fa7e7d9c01a048b4bb179ace5e0903a9b
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-05-27 19:37:53 +0300

    wip

---
+++ media/src/components/keyboard-shortcuts/v-keyboard-shortcuts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="v-keyboard-shortcuts">
+    <dl v-for="(item, index) in items" :key="index">
+      <dt>{{ item.title }}</dt>
+      <dd>
+        <v-keybinding-label :value="item.key" />
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-keyboard-shortcuts",
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style>
+@import "keyboard-shortcuts";
+</style>
--- media/tailwind.config.js
+++ media/tailwind.config.js
@@ -11,21 +11,46 @@ module.exports = {
     extend: {
       colors: {
         "focus-border": "var(--vscode-focusBorder)",
+
         "button-foreground": "var(--vscode-button-foreground)",
         "button-background": "var(--vscode-button-background)",
         "button-hover-background": "var(--vscode-button-hoverBackground)",
+
+        "keybinding-label-background": "var(--keybinding-label-background)",
+        "keybinding-label-foreground": "var(--keybinding-label-foreground)",
+        "keybinding-label-border": "var(--keybinding-label-border)",
+        "keybinding-label-bottom-border": "var(--keybinding-label-bottom-border)",
+
+        "keyboard-shortcuts-foreground": "var(--keyboard-shortcuts-foreground)"
+      },
+      spacing: {
+        "2sp": "0.125rem",
+        "3sp": "0.1875rem",
+        "5sp": "0.3125rem"
+      },
+      borderRadius: {
+        "3sp": "0.1875rem"
       },
       outline: {
         blue: ["1px solid var(--vscode-focusBorder)", "-1px"],
         button: ["1px solid var(--vscode-focusBorder)", "2px"],
       },
+      boxShadow: {
+        'keybinding-label': 'inset 0 -1px 0 var(--keybinding-label-shadow)',
+      },
+      fontSize: {
+        "11sp": "0.6875rem"
+      },
       fontFamily: {
         vscode: "var(--vscode-font-family)"
+      },
+      lineHeight: {
+        "10sp": "0.625rem"
       }
     }
   },
   variants: {
-    margin: ["responsive", "first"],
+    margin: ["responsive", "first", "last"],
     width: ["responsive"],
     outline: ["responsive", "focus-within", "focus", "active"],
   },
