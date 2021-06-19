3e64aef8252d6573a516311caa9f61eb3f8f4ff2
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-06-19 22:05:34 +0300

    fix codicon

--- src/counterEditor2/counter2EditorPanel.ts
+++ src/counterEditor2/counter2EditorPanel.ts
@@ -11,16 +11,13 @@ export class Counter2EditorPanel extends BaseEditorPanel<string> {
     const stylesPathOnDisk = vscode.Uri.joinPath(this.extensionUri, this.mediaFolderName, 'dist', 'assets', 'css', 'app.css');
     const stylesUri = webview.asWebviewUri(stylesPathOnDisk);
 
-    const codiconsFontPathOnDisk = vscode.Uri.joinPath(this.extensionUri, this.mediaFolderName, 'dist', 'assets', 'fonts', 'codicon.ttf');
-    const codiconsFontUri = webview.asWebviewUri(codiconsFontPathOnDisk);
-
     const nonce = getNonce();
 
     return `
       <html lang="en">
       <head>
         <meta charset="UTF-8">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource}; font-src ${codiconsFontUri}; script-src 'nonce-${nonce}';">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Counter Editor 2</title>
 
