6450302c2db26470436ab34a460c76065566f5e5
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-01-13 19:45:57 +0300

    refactor: rename

--- media/editor/src/router/index.ts
+++ media/editor/src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import editor from './editor';
 import inspector from './inspector';
-import system from './system';
+import notFound from './not-found';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [...editor, ...inspector, ...system]
+  routes: [...editor, ...inspector, ...notFound]
 });
 
 export default router;
