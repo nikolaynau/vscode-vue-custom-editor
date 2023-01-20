ba7c3a419a142c85edf034fe620215e836dda09a
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-01-20 22:42:13 +0300

    feat: add editor tests

--- media/editor/src/components/editor/__tests__/VEditor.spec.ts
+++ media/editor/src/components/editor/__tests__/VEditor.spec.ts
@@ -32,4 +32,57 @@ describe('VEditor', () => {
 
     expect(wrapper.find({ ref: 'output' }).text()).toBe('7');
   });
+
+  it('click plus button', async () => {
+    const value = JSON.stringify({ counter: 20 } as DocumentObject);
+    const wrapper = mount(VEditor, { props: { value } });
+
+    await Promise.all(
+      wrapper
+        .find('.v-editor__controls')
+        .findAll('.v-button')
+        .map(w => w.trigger('click'))
+    );
+
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('4');
+  });
+
+  it('clear value button', async () => {
+    const value = JSON.stringify({ counter: 20 } as DocumentObject);
+    const wrapper = mount(VEditor, { props: { value } });
+
+    await wrapper.find('.v-action').trigger('click');
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('0');
+  });
+
+  it('increment value on key up', async () => {
+    const wrapper = mount(VEditor, { props: { keyboardEnabled: true } });
+
+    await wrapper.find('.v-editor').trigger('keydown.up');
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('1');
+  });
+
+  it('decrement value on key down', async () => {
+    const wrapper = mount(VEditor, { props: { keyboardEnabled: true } });
+
+    await wrapper.find('.v-editor').trigger('keydown.down');
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('-1');
+  });
+
+  it('clear value on key delete', async () => {
+    const wrapper = mount(VEditor, { props: { keyboardEnabled: true } });
+
+    await wrapper.find('.v-editor').trigger('keydown.up');
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('1');
+
+    await wrapper.find('.v-editor').trigger('keyup.delete');
+    expect(wrapper.find({ ref: 'output' }).text()).toBe('0');
+  });
+
+  it('error editor value', async () => {
+    const value = JSON.stringify({ badCounter: 20 });
+    const wrapper = mount(VEditor, { props: { value } });
+
+    expect(wrapper.find('.v-editor__error').text()).toContain('invalid format');
+  });
 });
